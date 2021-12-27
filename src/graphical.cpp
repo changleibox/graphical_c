e30b7e1cd9a8daadb1209800f0ffc4d4f0007d2b
changlei <changlei@graspyun.com>
2021-12-27 15:41:29 +0800

    feat: 初始提交

--- src/graphical.cpp
+++ src/graphical.cpp
@@ -6,6 +6,37 @@
 
 using namespace std;
 
+struct Path {
+public:
+    const struct Incircle top;
+    const struct Incircle left;
+    const struct Incircle right;
+
+    Path(Incircle top, Incircle left, Incircle right) : top(std::move(top)), left(std::move(left)), right(std::move(right)) {}
+
+    string toJson() const {
+        return string()
+                .append("{")
+                .append("\"top\":")
+                .append("{" + top.toJson() + "}")
+                .append(",\"left\":")
+                .append("{" + left.toJson() + "}")
+                .append(",\"right\":")
+                .append("{" + right.toJson() + "}")
+                .append("}");
+    }
+
+    bool operator==(const Path &rhs) const {
+        return top == rhs.top &&
+               left == rhs.left &&
+               right == rhs.right;
+    }
+
+    bool operator!=(const Path &rhs) const {
+        return !(rhs == *this);
+    }
+};
+
 Incircle fromRadians(double radians, double radius) {
     return Incircle::fromRadians(radians, radius);
 }
--- src/graphical.h
+++ src/graphical.h
@@ -356,37 +356,6 @@ public:
     }
 };
 
-struct Path {
-public:
-    const struct Incircle top;
-    const struct Incircle left;
-    const struct Incircle right;
-
-    Path(Incircle top, Incircle left, Incircle right) : top(std::move(top)), left(std::move(left)), right(std::move(right)) {}
-
-    string toJson() const {
-        return string()
-                .append("{")
-                .append("\"top\":")
-                .append("{" + top.toJson() + "}")
-                .append(",\"left\":")
-                .append("{" + left.toJson() + "}")
-                .append(",\"right\":")
-                .append("{" + right.toJson() + "}")
-                .append("}");
-    }
-
-    bool operator==(const Path &rhs) const {
-        return top == rhs.top &&
-               left == rhs.left &&
-               right == rhs.right;
-    }
-
-    bool operator!=(const Path &rhs) const {
-        return !(rhs == *this);
-    }
-};
-
 Incircle fromRadians(double radians, double radius);
 
 Incircle fromSize(double width, double height, double radius, bool avoidOffset = false);
